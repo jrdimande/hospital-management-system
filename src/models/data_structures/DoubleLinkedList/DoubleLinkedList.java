743e0292daeaaf0cc77e4a441edebdc826625808
jrdimande <timoteojrdimande@gmail.com>
2026-03-31 13:18:39 +0200

    feat(DoubleLinkedList): removeByName() method added

--- src/models/data_structures/DoubleLinkedList/DoubleLinkedList.java
+++ src/models/data_structures/DoubleLinkedList/DoubleLinkedList.java
@@ -1,5 +1,7 @@
 package models.data_structures.DoubleLinkedList;
 
+import models.entities.Nameable;
+
 public class DoubleLinkedList {
     private Node head;
     private Node tail;
@@ -28,4 +30,43 @@ public class DoubleLinkedList {
     public int size(){
         return size;
     }
+
+    public void removeByName(String name){
+        Node current = this.head;
+
+        if (this.head == null && this.tail == null){
+            return;
+        }
+        while (current != null){
+            Object obj = current.getElement();
+            if (obj instanceof Nameable){
+                Nameable nameable = (Nameable) obj;
+
+                if (nameable.getName().equals(name)){
+                    Node prev = current.getPrev();
+                    Node next = current.getNext();
+
+                    if (prev != null){
+                        prev.setNext(next);
+                    }else {
+                        this.head = next;
+                    }
+
+                    if (next != null){
+                        next.setPrev(prev);
+                    }else {
+                        this.tail = prev;
+                    }
+
+                    this.size--;
+                    break;
+
+                }
+                current = current.getNext();
+            }
+        }
+
+
+    }
+    public void removeByID(){}
 }
