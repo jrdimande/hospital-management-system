2a2be9bf2d9a3306848a29645237ac0bbc02f55e
jrdimande <timoteojrdimande@gmail.com>
2026-04-02 19:24:39 +0200

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-import models.entities.Doctor;
-import models.entities.Nurse;
+import models.data_structures.DoubleLinkedList.DoubleLinkedList;
+import models.data_structures.Queue.Queue;
+import models.data_structures.Stack.Stack;
+import models.entities.*;
 
 public class Main {
     public static void main(String[] args){
